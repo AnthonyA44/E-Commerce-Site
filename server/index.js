b4e40c671f0b843e6889373cc269612042a5aeac
AnthonyA44 <anthonyalaimo12345@gmail.com>
2023-07-14 11:59:38 -0400

    added access control middleware

--- server/index.js
+++ server/index.js
@@ -18,6 +18,11 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(e);
     })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+    });
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
