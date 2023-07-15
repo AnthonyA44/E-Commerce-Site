d67805d60a78c940bf20ac2167ee21902b0a5b92
AnthonyA44 <anthonyalaimo12345@gmail.com>
2023-07-15 15:43:42 -0400

    base url updated

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://Anthony-Ecom-api.onrender.com";
+const BASE_URL = "https://localhost:4012";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
