a0a925d662867555cb93006bd3c328dfaf8602c0
AnthonyA44 <anthonyalaimo12345@gmail.com>
2023-07-15 21:45:50 -0400

    updated URL

--- client/src/Components/Products/Products.jsx
+++ client/src/Components/Products/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ category, filters, sort }) => {
       try {
         const res = await axios.get(
           category
-            ? `http://localhost:4012/api/products?category=${category}`
-            : "http://localhost:4012/api/products"
+            ? `https://anthony-ecom-api.onrender.com/api/products?category=${category}`
+            : "https://anthony-ecom-api.onrender.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://localhost:4012";
+const BASE_URL = "https://anthony-ecom-api.onrender.com";
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
